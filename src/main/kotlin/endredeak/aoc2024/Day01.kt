35cd90a72298970743f763bb241d0d5261ea99bd
Endre Deak <profendre@gmail.com>
2024-11-30 21:31:29 -0800

    Day 01

--- src/main/kotlin/endredeak/aoc2024/Day01.kt
+++ src/main/kotlin/endredeak/aoc2024/Day01.kt
@@ -1,15 +1,37 @@
 package endredeak.aoc2024
 
-fun main() {
-    solve("") {
-        val input = lines
+import kotlin.math.abs
 
-        part1(-1) {
-            -1
+fun main() {
+    solve("Historian Hysteria") {
+        val input = lines
+            .map { l -> l.split("   ").let { it[0].toInt() to it[1].toInt()} }
+            .let {
+                val (left, right) = mutableListOf<Int>() to mutableListOf<Int>()
+
+                it.forEach { (l, r) ->
+                    left.add(l)
+                    right.add(r)
+                }
+
+                left to right
+            }
+
+        part1(1873376) {
+            val (left, right) = input
+
+            left.sort()
+            right.sort()
+
+            left.indices.sumOf { i -> abs(left[i] - right[i]) }
         }
 
-        part2(-1) {
-            -1
+        part2(18997088) {
+            val (left, right) = input
+
+            left.sumOf { l ->
+                right.count { l == it } * l
+            }
         }
     }
 }

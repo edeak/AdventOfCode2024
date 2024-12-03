86a8873ae4ae3f796b38f458cde40761039bb6ad
Endre Deak <profendre@gmail.com>
2024-12-03 11:58:33 -0800

    Day 03

--- src/main/kotlin/endredeak/aoc2024/Day03.kt
+++ src/main/kotlin/endredeak/aoc2024/Day03.kt
@@ -1,15 +1,35 @@
 package endredeak.aoc2024
 
-fun main() {
-    solve("") {
-        val input = lines
+import endredeak.aoc2024.lib.utils.longs
 
-        part1(-1) {
-            -1
+fun main() {
+    solve("Mull It Over") {
+        val input = text
+        val regex = Regex("""(mul\(\d+,\d+\))|(do\(\))|(don't\(\))""")
+        fun String.calc() = this.longs().zipWithNext().first().let { (a, b) -> a * b }
+
+        part1(161289189) {
+            regex.findAll(input)
+                .mapNotNull { g -> g.value.takeIf { it.contains("mul") } }
+                .sumOf(String::calc)
         }
 
-        part2(-1) {
-            -1
+        part2(83595109) {
+            regex
+                .findAll(input)
+                .map { it.value }
+                .fold(0L to true) { acc, next ->
+                    when (next) {
+                        "don't()" -> acc.first to false
+                        "do()" -> acc.first to true
+                        else ->
+                            (if (acc.second) {
+                                next.longs().zipWithNext().first().let { (a, b) -> a * b }
+                            } else
+                                acc.first) to acc.second
+                    }
+                }
+                .first
         }
     }
 }

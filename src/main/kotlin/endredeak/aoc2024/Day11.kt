2d7ad41b85de4b9776502eb161d183239cc97990
Endre Deak <profendre@gmail.com>
2024-12-11 16:02:01 -0800

    Day 11

--- src/main/kotlin/endredeak/aoc2024/Day11.kt
+++ src/main/kotlin/endredeak/aoc2024/Day11.kt
@@ -1,15 +1,25 @@
 package endredeak.aoc2024
 
+import endredeak.aoc2024.lib.utils.longs
+
 fun main() {
-    solve("") {
-        val input = lines
+    solve("Plutonian Pebbles") {
+        val input = text
 
-        part1(-1) {
-            -1
-        }
+        fun Long.blink() =
+            when {
+                this == 0L -> listOf(1L)
+                "$this".length % 2 == 0 -> "$this".let { listOf(it.take(it.length / 2).toLong(), it.drop(it.length / 2).toLong()) }
+                else -> listOf(this * 2024L)
+            }
 
-        part2(-1) {
-            -1
-        }
+        val cache = mutableMapOf<Pair<Long, Int>, Long>()
+
+        fun calc(stone: Long, i: Int): Long =
+            if (i == 0) 1 else cache.getOrPut(stone to i) { stone.blink().sumOf { calc(it, i - 1) } }
+
+        part1(189167) { input.longs().sumOf { calc(it, 25) } }
+
+        part2(225253278506288) { input.longs().sumOf { calc(it, 75) } }
     }
 }

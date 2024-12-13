98de3780678e4fd330310f65fd3da2e06901705a
Endre Deak <profendre@gmail.com>
2024-12-13 07:10:36 -0800

    Day13 with utils

--- src/main/kotlin/endredeak/aoc2024/Day13.kt
+++ src/main/kotlin/endredeak/aoc2024/Day13.kt
@@ -1,15 +1,48 @@
 package endredeak.aoc2024
 
+import endredeak.aoc2024.lib.utils.doubles
+import endredeak.aoc2024.lib.utils.transpose
+import kotlin.math.ceil
+import kotlin.math.floor
+
 fun main() {
-    solve("") {
+    solve("Claw Contraption") {
+        fun Double.isInt() = floor(this) == ceil(this)
+
         val input = lines
+            .filter { it.isNotEmpty() }
+            .chunked(3)
+            .flatMap { eq -> eq.map { it.doubles() }.transpose() }
 
-        part1(-1) {
-            -1
-        }
+        // Cramer rule
+        fun solveSystem(first: List<Double>, second: List<Double>) =
+            first.let { (a1, b1, c1) ->
+                second.let { (a2, b2, c2) ->
+                    (a1 * b2 - b1 * a2)
+                        .takeIf { it != 0.0 }
+                        ?.let { det -> ((c1 * b2 - c2 * b1) / det) to ((a1 * c2 - a2 * c1) / det) }
+                }
+            }
 
-        part2(-1) {
-            -1
-        }
+        fun calc(max: Int? = null, mod: Double? = null) =
+            run {
+                mod?.let { m ->
+                    input
+                        .map { it.toMutableList().apply {
+                            this[2] += m
+                        }
+                    }
+                }
+                    ?: input
+            }
+                .chunked(2)
+                .mapNotNull { (f, s) -> solveSystem(f,s) }
+                .filter { (a, b) -> a.isInt() && b.isInt() }
+                .map { (a, b) -> a.toLong() to b.toLong() }
+                .let { max?.let { m -> it.filter { (a, b) -> a + b <= m } } ?: it }
+                .sumOf { (a, b) -> 3 * a + b }
+
+        part1(35255) { calc(max = 200) }
+        part2(87582154060429) { calc(mod = 10000000000000.0) }
     }
 }
--- src/main/kotlin/endredeak/aoc2024/lib/utils/Misc.kt
+++ src/main/kotlin/endredeak/aoc2024/lib/utils/Misc.kt
@@ -3,3 +3,4 @@ package endredeak.aoc2024.lib.utils
 
 fun String.ints() = Regex("""-?\d+""").findAll(this).map { it.value.toInt() }.toList()
 fun String.longs() = Regex("""-?\d+""").findAll(this).map { it.value.toLong() }.toList()
+fun String.doubles() = Regex("""-?\d+""").findAll(this).map { it.value.toDouble() }.toList()

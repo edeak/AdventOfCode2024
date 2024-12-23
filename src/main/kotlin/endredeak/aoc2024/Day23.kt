ddee82e9471c0ef24d832c53aa93ae99df9497ae
Endre Deak <profendre@gmail.com>
2024-12-23 00:24:21 -0800

    Day23

--- src/main/kotlin/endredeak/aoc2024/Day23.kt
+++ src/main/kotlin/endredeak/aoc2024/Day23.kt
@@ -1,15 +1,46 @@
 package endredeak.aoc2024
 
 fun main() {
-    solve("") {
-        val input = lines
+    solve("LAN Party") {
+        val input = lines.flatMap { it.split("-").let { (l, r) -> listOf(l to r, r to l) } }.let { pairs ->
+            val nodes = mutableMapOf<String, MutableSet<String>>()
 
-        part1(-1) {
-            -1
+            pairs.forEach { (l, r) ->
+                if (nodes.containsKey(l)) nodes[l]!!.add(r) else nodes[l] = mutableSetOf(r)
+                if (nodes.containsKey(r)) nodes[r]!!.add(l) else nodes[r] = mutableSetOf(l)
+            }
+
+            nodes
         }
 
-        part2(-1) {
-            -1
+        part1(1314) {
+            input
+                .flatMap { (k, vset) ->
+                    vset.toList().let { v ->
+                        v.indices.flatMap { i ->
+                            (i + 1..v.lastIndex).mapNotNull { j ->
+                                if (v[i] in input.getValue(v[j])) setOf(k, v[i], v[j]) else null
+                            }
+                        }
+                    }
+                }
+                .distinct()
+                .count { r -> r.any { it.startsWith("t") } }
+        }
+
+        part2("bg,bu,ce,ga,hw,jw,nf,nt,ox,tj,uu,vk,wp") {
+            input.map { (k, v) ->
+                var intersect = v + k
+                v.forEach { v1 ->
+                    (intersect intersect (input.getValue(v1) + v1)).let { next ->
+                        if (next.size > 2) intersect = next
+                    }
+                }
+                intersect
+            }
+                .maxBy { it.size }
+                .sorted()
+                .joinToString(",")
         }
     }
 }
